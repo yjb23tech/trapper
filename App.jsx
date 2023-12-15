5251e221fe749164440a35b6bde057e038e152f3
yjb23tech <yjb23tech@gmail.com>
2023-12-15 17:09:40 +0000

    All the way from London

--- App.jsx
+++ App.jsx
@@ -6,13 +6,13 @@ const App = () => {
         <div className="songs">
             <h1>Above It All</h1>
             <Card 
-                song_cover_art=""
+                song_cover_art="track_9_song_art.png"
                 song_title="Lost In The World"
-                album_icon=""
+                album_icon="album_icon.png"
                 album_name="My Beautiful Dark Twisted Fantasy"
-                artist_icon=""
+                artist_icon="artist_icon.png"
                 artist_name="Kanye West"
-                genre_icon=""
+                genre_icon="genre_icon.png"
                 genre_name="Baroque Rap"
             />
         </div>
